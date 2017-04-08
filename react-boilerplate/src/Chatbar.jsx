91ba06cdd7d4448866e90cee82e3672870ab33db
Jan Wong <janwong94@gmail.com>
2017-04-08 02:44:01 +0000

    redo latest git commit

--- react-boilerplate/src/Chatbar.jsx
+++ react-boilerplate/src/Chatbar.jsx
@@ -4,28 +4,37 @@ class Chatbar extends Component {
 
   constructor(props){
     super(props);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleKeyPressUsername = this.handleKeyPressUsername.bind(this);
+    this.handleKeyPressMessage = this.handleKeyPressMessage.bind(this);
+
   }
 
-  handleKeyPress(event) {
+  handleKeyPressUsername(event) {
+    if(event.key === 'Enter'){
+      this.props.onNewUsername(event.target.value);
+    }
+  }
+
+  handleKeyPressMessage(event) {
     if(event.key === 'Enter'){
       this.props.onNewMessage(event.target.value);
     }
   }
 
   render() {
-    const chatbarUsername = this.props.currentUser;
+    const chatbarUsername = this.props.currentUser.name;
     const chatbarMessage = 'Type a message and hit ENTER';
     return (
       <footer className="chatbar">
         <input
           className="chatbar-username"
           placeholder="Your Name (Optional)"
-          defaultValue={chatbarUsername} />
+          defaultValue={chatbarUsername}
+          onKeyPress={this.handleKeyPressUsername} />
         <input
           className="chatbar-message"
           placeholder="Type a message and hit ENTER"
-          onKeyPress={this.handleKeyPress} />
+          onKeyPress={this.handleKeyPressMessage} />
       </footer>
     );
   }
