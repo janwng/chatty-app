46ce7af5380c457deec120e26c0e0b1889bc68c3
Jan Wong <janwong94@gmail.com>
2017-04-08 21:24:21 +0000

    fix conflicts on master

--- react-boilerplate/src/App.jsx
+++ react-boilerplate/src/App.jsx
@@ -12,28 +12,7 @@ class App extends Component {
     };
 
     this.onNewMessage = this.onNewMessage.bind(this);
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // this.onNewUsername = this.onNewUsername.bind(this);
-=======
     this.onNewUsername = this.onNewUsername.bind(this);
->>>>>>> 2deb149eb62a8198cf4141651a71259fa4cff88f
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  // Function to change username
-  handleChange(event){
-    this.setState({currentUser.name: event.target.value});
-  }
-
-  // Function to output message if username is changed
-  handleSubmit(event) {
-    alert('Name got changed:' + this.state.currentUser.name);
-    event.preventDefault;
-=======
-    this.onNewUsername = this.onNewUsername.bind(this);
->>>>>>> switch-test
   }
 
   // Function to create new messages
@@ -47,17 +26,6 @@ class App extends Component {
     // Send the message to the chatty server
     this.socket.send(JSON.stringify(newMessage));
   }
-  
-  // onNewUsername(name) {
-  //   // let username = this.state.currentUser.name;
-  //   let newUsername =
-  //     {
-  //       type: 'postUsername',
-  //       username: name
-  //     }
-  //   // Send the message to the chatty server
-  //   this.socket.send(JSON.stringify(newUsername));
-  // }
 
   // Function to create new username
   onNewUsername(name) {
